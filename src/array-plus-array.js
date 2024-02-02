db24cae7bf3b86ae2009ec4d5a2ae9a4c96b7b52
Abel Yoshuara <abelyoshuara@gmail.com>
2024-02-02 16:03:36 +0800

    solve array-plus-array problem

---
+++ src/array-plus-array.js
@@ -0,0 +1,7 @@
+function arrayPlusArray(arr1, arr2) {
+  return [...arr1, ...arr2].reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]));
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]));
