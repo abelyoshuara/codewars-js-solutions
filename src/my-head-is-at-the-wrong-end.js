249d1a52b8c99bb9a5c75d07ab26891ad39fe903
Abel Yoshuara <abelyoshuara@gmail.com>
2024-02-13 08:54:48 +0800

    solve my head is at the wrong end problem

---
+++ src/my-head-is-at-the-wrong-end.js
@@ -0,0 +1,9 @@
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"]));
+console.log(fixTheMeerkat(["tails", "body", "heads"]));
+console.log(fixTheMeerkat(["bottom", "middle", "top"]));
+console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"]));
+console.log(fixTheMeerkat(["ground", "rainbow", "sky"]));
