efda4b1dbab56aa899f4bc6d5772df7ce4d6e59d
Abel Yoshuara <abelyoshuara@gmail.com>
2024-02-18 16:58:24 +0800

    solve beginner - reduce but grow problem

---
+++ src/beginner-reduce-but-grow.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-plusplus */
+function grow(x) {
+  let result = x[0];
+
+  for (let i = 1; i < x.length; i++) {
+    result *= x[i];
+  }
+
+  return result;
+}
+
+console.log(grow([1, 2, 3]), 6);
+console.log(grow([4, 1, 1, 1, 4]), 16);
+console.log(grow([2, 2, 2, 2, 2, 2]), 64);
